1a0585e4c5d5e479703c6cda1e36b00c79b9dd55
Erick Trevino <erick.trevino@cloudlite.io>
2025-08-27 14:34:49 -0500

    Add controller actions

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,23 +1,77 @@
 import type { Request, Response } from "express";
 import { PrismaClient } from '../../generated/prisma/index.js';
+import type { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 const prisma = new PrismaClient();
 
+type Task = {
+  id?: number;
+  title: string;
+  description?: string;
+  completed?: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 export const index = async(req: Request, res: Response) => {
   const allTasks = await prisma.task.findMany();
   res.status(200).json({ tasks: allTasks });
 };
 
-export const create = (req: Request, res: Response) => {
+export const create = async(req: Request, res: Response) => {
+  const { task }: { task: Task } = req.body;
 
-  res.status(200).json();
+  try {
+    const newTask = await prisma.task.create({
+      data: {
+        title: task.title,
+        description: task.description ?? null,
+        completed: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    })
+
+    res.status(201).json({ message: "Task created successfully", task: newTask });
+  } catch(e) {
+    res.status(422).json({ error: "Error when creating new task" })
+  }
 };
 
-export const update = (req: Request, res: Response) => {
-  res.status(200).json();
-};
+export const update = async(req: Request, res: Response) => {
+  const taskId: string | undefined = req.params.taskId;
+  const { task } = req.body;
 
-export const destroy = (req: Request, res: Response) => {
-  res.status(200).json();
+  try {
+    const updatedTask = await prisma.task.update({
+      where: {
+        id: Number(taskId)
+      },
+      data: {
+        title: task.title,
+        description: task.description,
+        updatedAt: new Date()
+      }
+    })
+
+    res.status(200).json({ message: "Task updated successfully", task: updatedTask});
+  } catch(e){
+    res.status(404).json({ error: `Error when updating task.`  })
+  }
+}
+
+export const destroy = async(req: Request, res: Response) => {
+  const taskId: string | undefined = req.params.taskId;
+
+  try {
+    await prisma.task.delete({
+      where: {
+        id: Number(taskId),
+      },
+    });
+
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch(e){
+    return res.status(422).json({ error: "Error when deleting task" })
+  }
 };
